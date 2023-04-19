83b39f90ae58be2642929fb033b7a4137d8809e0
Sid-Jbt <siddharth.k@jarvisbitz.com>
2023-04-19 10:15:52 +0530

    final commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '75fa4c2b-6250-47dc-97fb-36e2a2d3a164';
+export const MODAL_CLOSE_SEC = 2.5;
